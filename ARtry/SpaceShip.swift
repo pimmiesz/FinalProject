4a5ce053010abf1081dd3064b0ec61a601873d8e
pim <pimmiesz.p@gmail.com>
2019-02-27 13:48:10 +0700

    first commit

---
+++ ARtry/SpaceShip.swift
@@ -0,0 +1,13 @@
+//
+//  SpaceShip.swift
+//  ARtry
+//
+//  Created by Chawalya on 3/2/2562 BE.
+//  Copyright © 2562 Chawalya. All rights reserved.
+//
+
+import UIKit
+
+class SpaceShip: SCNNode {
+
+}
